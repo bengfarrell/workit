152ec9ed532a734f562b79130e62aae53b9e1cb5
ben <ben@benfarrell.com>
2014-05-08 22:09:07 -0700

    add functionality

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,38 @@
 
 angular.module('workitApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+
+        /** whether we are recording */
+        $scope.recording = false;
+
+        /** times recorded */
+        $scope.times = [];
+
+        /**
+         * toggle timer
+         */
+        $scope.timerToggle = function() {
+            $scope.recording = !$scope.recording;
+            if ($scope.recording == true) {
+                $scope.times.push( { start: new Date(), description: $scope.description } );
+            } else {
+                $scope.times[$scope.times.length-1].end = new Date();
+                $scope.times[$scope.times.length-1].description = $scope.description;
+            }
+        }
+
+        $scope.awesomeThings = [
+          'HTML5 Boilerplate',
+          'AngularJS',
+          'Karma'
+        ];
+  }).directive('currenttime', function() {
+        return {
+            restrict: 'E',
+            link: function (scope, element, attrs) {
+                setInterval(function() {
+                    element.html(new Date().toLocaleTimeString());
+                }, 1000);
+            }
+        }
+    });
